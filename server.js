a0fc9ddaf04351e7b7a8462ddf8a1776fc181e57
Adrian Ross <adriantoddross@gmail.com>
2018-01-30 15:15:10 -0500

    added error catcher-thingy

--- server.js
+++ server.js
@@ -5,16 +5,32 @@ const data = require('./db/notes');
 
 const { PORT } = require('./config');
 
+const { requestLogger } = require('./requestLogger');
+
 console.log('hello world!');
 
 const app = express();
 
 app.use(express.static('public'));
 
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
+});
+
+app.use(requestLogger);
+
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
 });
 
 app.get('/v1/notes', (req, res) => {
@@ -31,6 +47,12 @@ app.get('/v1/notes', (req, res) => {
 app.get('/v1/notes/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const foundID = data.find(item => item.id === id);
-  console.log(foundID);
+  // console.log(foundID);
   res.json(foundID);
+});
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'You got 404 problems but this page ain\'t one because it isn\'t real!' });
 });
