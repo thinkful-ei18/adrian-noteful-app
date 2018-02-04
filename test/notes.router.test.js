e24a4752b8ef2630f2c7deca0dd1d99bc4985772
Adrian Ross <adriantoddross@gmail.com>
2018-02-04 05:40:54 -0500

    test notesRouter DELETE method

--- test/notes.router.test.js
+++ test/notes.router.test.js
@@ -12,7 +12,7 @@ chai.use(spy);
 
 describe('notesRouter', function () {
 
-  it('should GET the entire list of notes', function () {
+  it('GETs  the entire list of notes', function () {
     return chai.request(app)
       .get('/v1/notes')
       .then(function (res) {
@@ -30,7 +30,21 @@ describe('notesRouter', function () {
       });
   });
 
-  it('should GET a specific note', function () {
+  // it('should return a 404 error when given a bad path', function () {
+  //   const spy = chai.spy();
+  //   return chai.request(app)
+  //     .get('/bad/path')
+  //     .then(spy)
+  //     .then(() => {
+  //       expect(spy).to.not.have.been.called();
+  //     })
+  //     .catch(err => {
+  //       expect(err.response).to.have.status(404);
+  //     });
+  // });
+
+
+  it('GETs a specific note', function () {
     const randomID = Math.floor(Math.random() * 10) + 1000;
     return chai.request(app)
       .get(`/v1/notes/${randomID}`)
@@ -43,47 +57,57 @@ describe('notesRouter', function () {
         expect(res.body.id).to.deep.equal(randomID);
       });
   });
+
+
+  it('modifies the `title` and `content` of a note', function () {
+    const updatedNote = {
+      title: 'How many cats does it take to change a lightbulb?',
+      content: 'A whole litter *ba-dum tssss*'
+    };
+
+    const randomID = Math.floor(Math.random() * 10) + 1000;
+    updatedNote.id = randomID;
+
+    return chai.request(app)
+      .put(`/v1/notes/${randomID}`)
+      .send(updatedNote)
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.deep.equal(updatedNote);
+      });
+  });
+
+  it('creates a new item', function () {
+    const newNote = {
+      title: 'A new era for cats',
+      content: 'A new day for catnip!'
+    };
+
+    return chai.request(app)
+      .post('/v1/notes/')
+      .send(newNote)
+      .then(function (res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.include.keys('title', 'content');
+
+        expect(res.body).to.deep.equal(Object.assign(newNote, {id: res.body.id}));
+      });
+  });
+
+  it('permanently deletes an item', function () {
+    const randomID = Math.floor(Math.random() * 10) + 1000;
+
+    return chai.request(app)
+      .delete(`/v1/notes/${randomID}`)
+      .then(function (res) {
+        expect(res).to.have.status(204);
+      });
+  });
+
 });
-
-it('should modify the `title` and `content` of a note', function () {
-  const updatedNote = {
-    title: 'How many cats does it take to change a lightbulb?',
-    content: 'A whole litter *ba-dum tssss*'
-  };
-
-  const randomID = Math.floor(Math.random() * 10) + 1000;
-  updatedNote.id = randomID;
-
-  return chai.request(app)
-    .put(`/v1/notes/${randomID}`)
-    .send(updatedNote)
-    .then(function(res) {
-      expect(res).to.have.status(200);
-      expect(res).to.be.json;
-      expect(res.body).to.be.a('object');
-      expect(res.body).to.deep.equal(updatedNote);
-    });
-});
-
-it('should create a new item', function () {
-  const newNote = {
-    title: 'A new era for cats',
-    content: 'A new day for catnip!'
-  };
-
-  return chai.request(app)
-    .post('/v1/notes/')
-    .send(newNote)
-    .then(function (res) {
-      expect(res).to.have.status(201);
-      expect(res).to.be.json;
-      expect(res.body).to.be.a('object');
-
-      expect(res.body.id).to.not.equal(null);
-      expect(res.body).to.include.keys('title', 'content');
-
-      expect(res.body).to.deep.equal(Object.assign(newNote, {id: res.body.id}));
-    });
-});
-
-
