c202ed8461610aeefaa4a64d7c43ea76c9ad4cbc
Adrian Ross <adriantoddross@gmail.com>
2018-02-05 15:44:42 -0500

    create table, start with id 1000

---
+++ new-db/noteful-app.1.sql
@@ -0,0 +1,19 @@
+-- Test if you have access to the database.
+-- SELECT CURRENT_DATE;
+
+-- CREATE TABLE notes
+-- (id serial PRIMARY KEY,
+-- title text NOT NULL,
+-- content text,
+-- created date DEFAULT current_timestamp);
+
+-- DROP TABLE notes;
+
+-- Start notes with ID 1000 - BONUS CHALLENGE!
+-- ALTER SEQUENCE notes_id_seq RESTART WITH 1000 INCREMENT BY 1;
+
+
+
+-- INSERT INTO notes
+-- (title, content) VALUES
+-- ()
